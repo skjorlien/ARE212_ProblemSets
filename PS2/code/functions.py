c874c6a4b3a06c8ebc5fd8301e85f191190289d8
Scott Kjorlien <skjorlien@gmail.com>
2023-04-15 12:32:02 -0700

    combine Sarah & Scott's Q4

--- PS2/code/functions.py
+++ PS2/code/functions.py
@@ -1,14 +1,14 @@
 from scipy.stats import distributions as iid
+from scipy.stats import multivariate_normal
 import numpy as np 
+import matplotlib.pyplot as plt
+from classes import Two_SLS
 
 
 def dgp(n, β, π):
     ## Generate errors 
-    u = iid.norm(0, 1)
-    v = iid.norm(0, 1)
-    Z = iid.norm(0, 1)
-    u = u.rvs(n).reshape(-1, 1)
-    v = v.rvs(n).reshape(-1,1)
+    u = iid.norm().rvs(size = (n, 1))
+    v = iid.norm().rvs(size= (n, 1))
 
     ## the number of instruments to generate = number of elements in pi
     if type(π) == np.ndarray:
@@ -16,27 +16,15 @@ def dgp(n, β, π):
             π = π.reshape(-1, 1)
     else: 
         π = np.array([π]).reshape(-1,1)
-    d = π.size
-    Z = Z.rvs(n*d).reshape(n, d)
+    l = π.size
+
+    # create x, y, Z
+    Z = multivariate_normal(np.zeros(l), np.eye(l)).rvs(size=n)
+    if Z.ndim == 1: 
+        Z = Z.reshape(-1, 1)
     x = Z@π + v
     y = β*x + u 
     return (y, x, Z) 
-
-
-def two_sls(y, x, Z): 
-    π = np.linalg.solve(Z.T@Z, Z.T@x)
-    xhat = Z@π
-    β = np.linalg.solve(xhat.T@xhat, xhat.T@y)
-    # calculate variance
-    df = Z.shape[0] - Z.shape[1]
-    e = y - x@β
-    V = compute_variance(e, xhat, df)
-    return β, V
-
-
-def ttest(est, se, val = 0):
-    t = (est - val)/se
-    return t
 
 
 def hansen(x, y, Z, b0):
@@ -53,9 +41,27 @@ def hansen(x, y, Z, b0):
     p = 2*(1 - iid.f.cdf(fstat, df1, df2))
     return p
 
-def compute_variance(e, X, df):
-    s2 = e.T@e / df
-    V = s2 * np.linalg.inv(X.T@X)
-    return V
 
+n = 1000
+pi = np.linspace(0.001,1,9)
+fig, ax = plt.subplots(3,3)
+ax = ax.reshape(-1)
+for i, coef in enumerate(pi): 
+    b = np.empty(0)
+    se = np.empty(0)
+    for j in range(1000):
+        x, y, Z = dgp(n, β, π)
+        model = Two_SLS(y, x, Z)
+        b = np.append(b, model.beta_tsls)
+        # se = np.append(se, model.se())
 
+    
+    monte_carlo_5 = np.percentile(b, 5)
+    monte_carlo_95 = np.percentile(b, 95)
+
+    ax[i].hist(b)
+    ax[i].set_title(f"Pi = {np.round(coef, 2)}")
+    ax[i].axvline(x=1, color='r', label='True beta')
+    ax[i].axvline(x = monte_carlo_5, color = 'b')
+    ax[i].axvline(x = monte_carlo_95, color = 'b')
+fig.tight_layout()
